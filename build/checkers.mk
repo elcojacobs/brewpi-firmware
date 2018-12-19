b3820978a31e3c9b3ee147f53b6a6d0bb5d6510a
Elco Jacobs <elco@brewpi.com>
2018-12-19 23:17:11 +0100

    fix invalid pointer for ticks block in testbox

--- build/checkers.mk
+++ build/checkers.mk
@@ -17,46 +17,47 @@ CFLAGS += -fsanitize=address
 CFLAGS += -fsanitize=undefined
 
 # try to keep running
-CFLAGS += -fsanitize-recover=all
+# CFLAGS += -fsanitize-recover=all
 
-# CFLAGS += -fsanitize=shift
+CFLAGS += -fsanitize=shift
 # This option enables checking that the result of a shift operation is not undefined. Note that what exactly is considered undefined differs slightly between C and C++, as well as between ISO C90 and C99, etc. This option has two suboptions, CFLAGS += -fsanitize=shift-base and CFLAGS += -fsanitize=shift-exponent.
 
-# CFLAGS += -fsanitize=shift-exponent
+CFLAGS += -fsanitize=shift-exponent
 # This option enables checking that the second argument of a shift operation is not negative and is smaller than the precision of the promoted first argument.
 
-# CFLAGS += -fsanitize=shift-base
+CFLAGS += -fsanitize=shift-base
 # If the second argument of a shift operation is within range, check that the result of a shift operation is not undefined. Note that what exactly is considered undefined differs slightly between C and C++, as well as between ISO C90 and C99, etc.
 
-# CFLAGS += -fsanitize=integer-divide-by-zero
+CFLAGS += -fsanitize=integer-divide-by-zero
 # Detect integer division by zero as well as INT_MIN / -1 division.
 
-# CFLAGS += -fsanitize=unreachable
+CFLAGS += -fsanitize=unreachable
 # With this option, the compiler turns the __builtin_unreachable call into a diagnostics message call instead. When reaching the __builtin_unreachable call, the behavior is undefined.
 
-# CFLAGS += -fsanitize=vla-bound
+CFLAGS += -fsanitize=vla-bound
 # This option instructs the compiler to check that the size of a variable length array is positive.
 
-# CFLAGS += -fsanitize=null
+CFLAGS += -fsanitize=null
 # This option enables pointer checking. Particularly, the application built with this option turned on will issue an error message when it tries to dereference a NULL pointer, or if a reference (possibly an rvalue reference) is bound to a NULL pointer, or if a method is invoked on an object pointed by a NULL pointer.
 
-# CFLAGS += -fsanitize=return
+CFLAGS += -fsanitize=return
 # This option enables return statement checking. Programs built with this option turned on will issue an error message when the end of a non-void function is reached without actually returning a value. This option works in C++ only.
 
-# CFLAGS += -fsanitize=signed-integer-overflow
+CFLAGS += -fsanitize=signed-integer-overflow
 # This option enables signed integer overflow checking. We check that the result of +, *, and both unary and binary - does not overflow in the signed arithmetics. Note, integer promotion rules must be taken into account. That is, the following is not an overflow:
 # signed char a = SCHAR_MAX;
 # a++;
-# CFLAGS += -fsanitize=bounds
+
+CFLAGS += -fsanitize=bounds
 # This option enables instrumentation of array bounds. Various out of bounds accesses are detected. Flexible array members, flexible array member-like arrays, and initializers of variables with static storage are not instrumented.
 
 # CFLAGS += -fsanitize=bounds-strict
 # This option enables strict instrumentation of array bounds. Most out of bounds accesses are detected, including flexible array members and flexible array member-like arrays. Initializers of variables with static storage are not instrumented.
 
-# CFLAGS += -fsanitize=alignment
+CFLAGS += -fsanitize=alignment
 # This option enables checking of alignment of pointers when they are dereferenced, or when a reference is bound to insufficiently aligned target, or when a method or constructor is invoked on insufficiently aligned object.
 
-# CFLAGS += -fsanitize=object-size
+CFLAGS += -fsanitize=object-size
 # This option enables instrumentation of memory references using the __builtin_object_size function. Various out of bounds pointer accesses are detected.
 
 # CFLAGS += -fsanitize=float-divide-by-zero
@@ -71,13 +72,13 @@ CFLAGS += -fsanitize-recover=all
 # CFLAGS += -fsanitize=returns-nonnull-attribute
 # This option enables instrumentation of return statements in functions marked with returns_nonnull function attribute, to detect returning of null values from such functions.
 
-# CFLAGS += -fsanitize=bool
+CFLAGS += -fsanitize=bool
 # This option enables instrumentation of loads from bool. If a value other than 0/1 is loaded, a run-time error is issued.
 
-# CFLAGS += -fsanitize=enum
+CFLAGS += -fsanitize=enum
 # This option enables instrumentation of loads from an enum type. If a value outside the range of values for the enum type is loaded, a run-time error is issued.
 
-# CFLAGS += -fsanitize=vptr
+CFLAGS += -fsanitize=vptr
 # This option enables instrumentation of C++ member function calls, member accesses and some conversions between pointers to base and derived classes, to verify the referenced object has the correct dynamic type.
 
 # CFLAGS += -fsanitize=pointer-overflow
